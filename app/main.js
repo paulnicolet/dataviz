7f0f9d97942da32316826be9964ed5bc9251d1f1
paulnicolet <nicoletpaul@orange.fr>
2017-11-15 18:46:05 +0100

    Link slider to map

--- app/main.js
+++ app/main.js
@@ -8,15 +8,17 @@ require('./main.scss');
 const SLIDER_ID = 'slider';
 const SLIDER_WIDTH = 900;
 const SLIDER_HEIGHT = 100;
-const START_DATE = new Date('1900');
-const END_DATE = new Date('2017');
-buildSlider(SLIDER_ID, START_DATE, END_DATE, SLIDER_WIDTH, SLIDER_HEIGHT);
-
+const START_DATE = new Date('1745');
+const END_DATE = new Date('2013');
+let slider = buildSlider(SLIDER_ID, START_DATE, END_DATE, SLIDER_WIDTH, SLIDER_HEIGHT);
 
 // Build map
 const MAP_ID = 'map';
 const MAP_WIDTH = 900;
 const MAP_HEIGHT = 600;
 const WORLD_TOPOJSON_PATH = 'https://unpkg.com/world-atlas@1/world/110m.json';
-const TEMPERATURES_PATH = './data/temp_city_1900-01-01_.json';
-buildMap(MAP_ID, TEMPERATURES_PATH, WORLD_TOPOJSON_PATH, MAP_WIDTH, MAP_HEIGHT);
+const TEMPERATURES_PATH = './data/temp_city_all.json';
+let map = buildMap(MAP_ID, TEMPERATURES_PATH, WORLD_TOPOJSON_PATH, MAP_WIDTH, MAP_HEIGHT, slider);
+
+// Register with slider
+slider.moved(year => map.renderTemperatures(year));
